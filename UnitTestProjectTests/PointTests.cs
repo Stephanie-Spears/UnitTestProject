ab5d0cc0231d565ba0728766b42e1415295cc8fd
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-02 01:09:29 -0700

    add unit test for point methods

--- UnitTestProjectTests/PointTests.cs
+++ UnitTestProjectTests/PointTests.cs
@@ -10,16 +10,38 @@ namespace UnitTestProject.Tests
 {
     public class PointTests
     {
-        [Fact()]
+        [Fact]
         public void PointTest()
         {
-            Assert.True(false, "This test needs an implementation");
+            int x = 5;
+            int y = 6;
+
+            var point = new Point(x, y);
+
+            Assert.Equal(x, point.X);
+            Assert.Equal(y, point.Y);
         }
 
         [Fact()]
-        public void DistanceToTest()
+        public void DistanceToWithPathagoreanTriple()
         {
-            Assert.True(false, "This test needs an implementation");
+            var point = new Point(3, 4);
+            var target = new Point(0, 0);
+            var expected = 5.0;
+            var actual = target.DistanceTo(point);
+
+            Assert.Equal(expected, actual, 2);
+        }
+
+        [Fact]
+        public void DistanceToPointAtSamePosition()
+        {
+            var point = new Point(3, 4);
+            var target = new Point(3, 4);
+            var expected = 0.0;
+
+            var actual = target.DistanceTo(point);
+            Assert.Equal(expected, actual, 2);
         }
     }
 }
