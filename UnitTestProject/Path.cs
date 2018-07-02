c8ce3ff90dd5380d806508da73f320afbca77123
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-02 01:47:30 -0700

    behavior driven test development

---
+++ UnitTestProject/Path.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UnitTestProject
+{
+    public class Path
+    {
+        private MapLocation[] pathLocations;
+
+        public Path(MapLocation[] pathLocations)
+        {
+            this.pathLocations = pathLocations;
+        }
+
+        public bool IsOnPath(MapLocation mapLocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
