3a6a2a7c0e7e2c6ea80f72f6b853285e818668ff
Stephanie Spears <stephanie.spears1@gmail.com>
2018-07-02 00:45:36 -0700

    generate unit test for point class

---
+++ UnitTestProjectTests/PointTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using UnitTestProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.Tests
+{
+    public class PointTests
+    {
+        [Fact()]
+        public void PointTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DistanceToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
